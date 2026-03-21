716aac2190f62fb92eb1bac7411aa999e7a47ac0
Tanut Pensuk <Tanut.P@Tanuts-MacBook-Air.local>
2026-03-21 12:20:51 +0700

    (feat) add gitlab helm

--- harbor/install.sh
+++ harbor/install.sh
@@ -1,3 +1,4 @@
 helm repo add harbor https://helm.goharbor.io
 helm repo update
 helm install my-harbor harbor/harbor -f harbor-local.yaml -n harbor --create-namespace
+helm upgrade --install my-harbor harbor/harbor -f harbor-local.yaml -n harbor
