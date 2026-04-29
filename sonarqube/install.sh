a359d3c13001f54492c44fbdf6a1af39c16c297d
Tanut <tanut.p@ibmdt.co.th>
2026-04-29 11:18:39 +0700

    refactor: simplify SonarQube installation by removing unnecessary parameters and creating namespace

--- sonarqube/install.sh
+++ sonarqube/install.sh
@@ -2,6 +2,4 @@
 
 helm repo add sonarqube https://SonarSource.github.io/helm-chart-sonarqube
 helm repo update
-kubectl create namespace sonarqube
-export MONITORING_PASSCODE="admin123"
-helm upgrade --install -n sonarqube sonarqube sonarqube/sonarqube --set community.enabled=true,monitoringPasscode=$MONITORING_PASSCODE
+helm upgrade --install -n sonarqube sonarqube sonarqube/sonarqube -f sonarqube-values.yaml --create-namespace
