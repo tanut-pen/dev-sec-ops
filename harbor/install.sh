0dcf23a7a217dc046300263666602d4da5b52e55
Tanut <tanut.p@ibmdt.co.th>
2026-04-29 11:26:07 +0700

    update: change harbor configuration file reference and generate new values file

--- harbor/install.sh
+++ harbor/install.sh
@@ -2,3 +2,5 @@ helm repo add harbor https://helm.goharbor.io
 helm repo update
 helm install my-harbor harbor/harbor -f harbor-local.yaml -n harbor --create-namespace
 helm upgrade --install my-harbor harbor/harbor -f harbor-local.yaml -n harbor
+
+helm show values harbor/harbor > harbor-values.yaml
