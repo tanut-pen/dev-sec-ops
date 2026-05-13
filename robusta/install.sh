70fb7fd033550a70b92bb4dfd46bdc0eac86fcb5
Tanut <tanut.p@ibmdt.co.th>
2026-05-13 11:44:03 +0700

    remove robusta application configuration and related files

--- robusta/install.sh
+++
@@ -1,4 +0,0 @@
-helm repo add robusta https://robusta-charts.storage.googleapis.com
-helm repo update
-helm install robusta robusta/robusta -f robusta-values.yaml
-helm show values robusta/robusta > robusta-values.yaml
