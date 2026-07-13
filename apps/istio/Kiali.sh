88c0bb933db6f50b4e245f6fa55bef9a0c3cd778
Tanut <tanut.p@ibmdt.co.th>
2026-07-13 09:19:46 +0700

    chore: remove ingressClassName from multiple ingress resources

--- apps/istio/Kiali.sh
+++ apps/istio/Kiali.sh
@@ -1,2 +1,5 @@
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.21/samples/addons/prometheus.yaml
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.21/samples/addons/kiali.yaml
+
+kubectl delete -f https://raw.githubusercontent.com/istio/istio/release-1.21/samples/addons/kiali.yaml
+kubectl delete -f https://raw.githubusercontent.com/istio/istio/release-1.21/samples/addons/prometheus.yaml
