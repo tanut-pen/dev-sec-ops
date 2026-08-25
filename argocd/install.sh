22b5e9e8a34be597a28cf1da980bfd47c9a38c17
tanut.ibmdt <tanut.p@ibmdt.co.th>
2026-08-25 14:31:56 +0700

    refactor: extract demo-services ArgoCD app into separate manifest and fix install.sh formatting

--- argocd/install.sh
+++ argocd/install.sh
@@ -14,10 +14,10 @@ helm upgrade --install argo-rollouts argo/argo-rollouts \
   --create-namespace \
   -f values-rollouts.yaml
 
+echo "here is the initial password for the admin user:"
+
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 
-
 kubectl --kubeconfig=path/to/config-rancher-lab config get-contexts
-
 
 export KUBECONFIG=~/.kube/config:~/.kube/config-rancher-lab
